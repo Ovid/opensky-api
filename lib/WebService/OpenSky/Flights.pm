ffc5648360b840e5d72384124c59e478ebdf3ee5
Ovid <ovid@cpan.org>
2023-06-01 09:35:56 +0200

    Response subclasses should only care about responses, not the iterator

--- lib/WebService/OpenSky/Flights.pm
+++ lib/WebService/OpenSky/Flights.pm
@@ -5,13 +5,11 @@ package WebService::OpenSky::Flights;
 our $VERSION = '0.005';
 use Moose;
 use WebService::OpenSky::Core::Flight;
-use WebService::OpenSky::Utils::Iterator;
 use experimental qw(signatures);
 extends 'WebService::OpenSky::Response';
 
-sub _create_response_iterator ($self) {
-    my @responses = map { WebService::OpenSky::Core::Flight->new($_) } $self->raw_response->@*;
-    return WebService::OpenSky::Utils::Iterator->new( rows => \@responses );
+sub _create_response_objects ($self) {
+    return [ map { WebService::OpenSky::Core::Flight->new($_) } $self->raw_response->@* ];
 }
 
 # trusted method only called by WebService::OpenSky
--- lib/WebService/OpenSky/States.pm
+++ lib/WebService/OpenSky/States.pm
@@ -5,13 +5,11 @@ package WebService::OpenSky::States;
 our $VERSION = '0.005';
 use Moose;
 use WebService::OpenSky::Core::StateVector;
-use WebService::OpenSky::Utils::Iterator;
 use experimental qw(signatures);
 extends 'WebService::OpenSky::Response';
 
-sub _create_response_iterator ($self) {
-    my @responses = map { WebService::OpenSky::Core::StateVector->new($_) } $self->raw_response->{states}->@*;
-    return WebService::OpenSky::Utils::Iterator->new( rows => \@responses );
+sub _create_response_objects ($self) {
+    return [ map { WebService::OpenSky::Core::StateVector->new($_) } $self->raw_response->{states}->@* ];
 }
 
 # trusted method only called by WebService::OpenSky
