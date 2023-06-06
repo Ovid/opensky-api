e91ad23a307c8422233c434a81322423ff23ae31
Ovid <ovid@cpan.org>
2023-06-06 08:15:55 +0200

    We no longer need to exclude StrictConstructor

--- lib/WebService/OpenSky/Moose.pm
+++ lib/WebService/OpenSky/Moose.pm
@@ -3,6 +3,7 @@ package WebService::OpenSky::Moose;
 # ABSTRACT: MooseX::Extended::Custom for WebService::OpenSky
 
 use MooseX::Extended::Custom;
+use PerlX::Maybe 'provided';
 our $VERSION = '0.4';
 
 # If $^P is true, we're running under the debugger.
@@ -11,24 +12,10 @@ our $VERSION = '0.4';
 # because the way the debugger works with B::Hooks::AtRuntime will cause
 # the class to be made immutable before the we apply everything we need. This
 # causes the code to die.
-sub import {
-    my ( $class, %args ) = @_;
-
-    # we exclude strictconstructor because, otherwise, the delayed param trick
-    # for OpenSky::API::Core will fail.
-    my @excludes = 'StrictConstructor';
-    if ($^P) {
-        state $warned;
-        unless ($warned) {
-            $warned++;
-            say STDERR "Running under the debugger. Excluding make_immutable\n";
-        }
-        push @excludes => 'immutable';
-    }
-    push @excludes => 'immutable' unless $^P;
+sub import ( $class, %args ) {
     MooseX::Extended::Custom->create(
-        includes => [ 'method', 'try' ],
-        excludes => \@excludes,
+        includes => 'method',
+        provided $^P, excludes => 'immutable',
         %args    # you need this to allow customization of your customization
     );
 }
